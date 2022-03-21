87ec5babaf0705858eed815b228345e829c1c145
Clifford Tse <TseClifford@gmail.com>
2022-03-21 15:40:42 -0400

    add content field in notes schema

--- backend/db/seeds/03_notes.sql
+++ backend/db/seeds/03_notes.sql
@@ -1,6 +1,6 @@
-INSERT INTO notes (title, notebook_id) VALUES ('Note1', 1);
-INSERT INTO notes (title, notebook_id) VALUES ('Note2', 2);
-INSERT INTO notes (title, notebook_id) VALUES ('Note3', 3);
-INSERT INTO notes (title, notebook_id) VALUES ('Note4', 4);
-INSERT INTO notes (title, notebook_id) VALUES ('Note5', 5);
-INSERT INTO notes (title, notebook_id) VALUES ('Note6', 6);
+INSERT INTO notes (title, content, notebook_id) VALUES ('Note1', '##Sample text', 1);
+INSERT INTO notes (title, content, notebook_id) VALUES ('Note2', '##Sample text', 2);
+INSERT INTO notes (title, content, notebook_id) VALUES ('Note3', '##Sample text', 3);
+INSERT INTO notes (title, content, notebook_id) VALUES ('Note4', '##Sample text', 4);
+INSERT INTO notes (title, content, notebook_id) VALUES ('Note5', '##Sample text', 5);
+INSERT INTO notes (title, content, notebook_id) VALUES ('Note6', '##Sample text', 6);
