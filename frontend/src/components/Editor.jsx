c8143dd7b60a0f08e01122ddc9833aff3c2aecba
Donatas Tubutis <33428886+dktrdktr@users.noreply.github.com>
2022-03-21 16:56:07 -0400

    install headless ui, clean up editor

--- frontend/src/components/Editor.jsx
+++ frontend/src/components/Editor.jsx
@@ -4,23 +4,6 @@ import MarkedPreview from "./MarkedPreview";
 const Editor = () => {
   return (
     <>
-      <h1 class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg transition duration-75 hover:bg-gray-100 dark:hover:bg-gray-700 dark:text-white group">
-        <svg
-          class="w-6 h-6 dark:text-white"
-          fill="none"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"
-          ></path>
-        </svg>
-        <span class="ml-3">Editor</span>
-      </h1>
       <MarkedInput />
       <MarkedPreview />
     </>
