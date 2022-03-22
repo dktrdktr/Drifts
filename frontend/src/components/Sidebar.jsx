f817614c67410e499d88ba66cfcb166350f4ae6d
Clifford Tse <TseClifford@gmail.com>
2022-03-22 00:42:49 -0400

    refactor components layout

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,26 +1,16 @@
 import NotebookList from "./NotebookList";
+import NoteList from "./NoteList";
 
 const Sidebar = () => {
   return (
     <>
-      <h1 class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg transition duration-75 hover:bg-gray-100 dark:hover:bg-gray-700 dark:text-white group">
-        <svg
-          class="w-6 h-6 dark:text-white"
-          fill="none"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"
-          ></path>
-        </svg>
-        <span class="ml-3">Notebooks</span>
-      </h1>
-      <NotebookList />
+      <div className="w-2/12 h-full p-2 border-y-2 border-l-2">
+        <NotebookList />
+      </div>
+
+      <div className="w-3/12 h-full p-2 border-y-2 border-l-2">
+        <NoteList />
+      </div>
     </>
   );
 };
