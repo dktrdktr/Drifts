a92fca0a6ae29d1c36228a54f53144751db75eff
Clifford Tse <TseClifford@gmail.com>
2022-03-25 14:05:14 -0400

    update notes seed data

--- backend/db/seeds/03_notes.sql
+++ backend/db/seeds/03_notes.sql
@@ -1,9 +1,102 @@
-INSERT INTO notes (title, content, notebook_id) VALUES ('Notebook1 - Note1', '## Notebook1 - Note1', 1);
-INSERT INTO notes (title, content, notebook_id) VALUES ('Notebook1 - Note2', '## Notebook1 - Note2', 1);
-INSERT INTO notes (title, content, notebook_id) VALUES ('Notebook1 - Note3', '## Notebook1 - Note3', 1);
-INSERT INTO notes (title, content, notebook_id) VALUES ('Notebook2 - Note1', '## Notebook2 - Note1', 2);
-INSERT INTO notes (title, content, notebook_id) VALUES ('Notebook2 - Note2', '## Notebook2 - Note2', 2);
-INSERT INTO notes (title, content, notebook_id) VALUES ('Notebook3 - Note1', '## Notebook3 - Note1', 3);
-INSERT INTO notes (title, content, notebook_id) VALUES ('Notebook4 - Note1', '## Notebook4 - Note1', 4);
-INSERT INTO notes (title, content, notebook_id) VALUES ('Notebook5 - Note1', '## Notebook5 - Note1', 5);
-INSERT INTO notes (title, content, notebook_id) VALUES ('Notebook6 - Note1', '## Notebook6 - Note1', 6);
+INSERT INTO notes (title, content, notebook_id)
+VALUES (
+    'Crispy Carrots',
+    '## Crispy Carrots
+
+Ingredients:
+- 1 lb carrot sticks (or baby carrots)
+- 2 oranges (or any citrus)
+- 2 tbsp butter
+- 1 package phyllo dough
+- 1/2 cup honey
+- sesame seeds to top
+- vegetable oil
+
+Directions:
+- Start by parboiling your carrots in 2 cups of water (means party boil).
+- Zest and juice the oranges.
+- When the carrots are jussst soft, drain the water. Add the butter, the zest, and the orange juice. Simmer until thick and declious. It'' s fine (
+      and good
+    ) if the carrots are still a bit undercooked (
+      we ''re going to bake them next).
+- Unroll the phyllo dough and cut it into three equal sections. With a brush, oil the top of each sheet.
+- Roll a carrot with a single section of phyllow dough, place on a baking sheet. Repeat and repeat and repeat.
+- When a full baking sheet is ready, brush the top of each roll with a little oil. Don'' t worry about placing them too far about,
+      they won ''t raise much (at all).
+- Bake according to your phyllo dough box.
+- With 5 minutes left, brush the top of each roll with honey, then sprinkle with sesame seeds.',
+    1
+  );
+INSERT INTO notes (title, content, notebook_id)
+VALUES (
+    'Banana Bread',
+    '## Banana Bread
+
+Ingredients:
+- 4 bananas
+- 1/2 cup butter
+- 1/2 cup sugar
+- 2 eggs
+- 2 cups flour
+- 1/2 tsp baking soda
+- 1 tsp baking powder
+- pinch salt
+- 1/4 cup nuts (we like pecans)
+
+Directions:
+- Beat the eggs, then cream with the butter and sugar
+- Mix in bananas, then flour, baking soda/powder, salt, and nuts
+- Add to greased and floured pan
+- Bake until brown/cracked, toothpick comes out clean',
+    1
+  );
+INSERT INTO notes (title, content, notebook_id)
+VALUES (
+    'Stuffed Peppers',
+    '## Stuffed Peppers
+
+Ingredients:
+- 1 jar Peppadew Peppers (or piquante pepper)
+- 4oz goat cheese (any flavor)
+- 1 tbsp mayonnaise
+- 1 tbsp sour cream
+- 1 bunch of chives, chopped
+- hearty shot of hot sauce (Franks, Yellowbird)
+- hearty crack of pepper
+
+Directions:
+- combine goat cheese, mayo, sour cream, 2/3rds of your chives, hot sauce, black pepper
+- if you are feeling spry, beat the mixture to make it fluffy
+- put filling in a plastic bag, snip of the tip with scissors to make a piping bag
+- fill peppers, place in bowl, top with chives and hot sauce',
+    1
+  );
+INSERT INTO notes (title, content, notebook_id)
+VALUES (
+    'March 20',
+    '## March 20, 2022
+- I wrote some code',
+    2
+  );
+INSERT INTO notes (title, content, notebook_id)
+VALUES (
+    'March 21',
+    '## March 21, 2022
+- I wrote more code',
+    2
+  );
+INSERT INTO notes (title, content, notebook_id)
+VALUES (
+    'Encanto',
+    '### Storyline
+Encanto tells the tale of an extraordinary family, the Madrigals, who live hidden in the mountains of Colombia, in a magical house, in a vibrant town, in a wondrous, charmed place called an Encanto. The magic of the Encanto has blessed every child in the family with a unique gift from super strength to the power to heal-every child except one, Mirabel. But when she discovers that the magic surrounding the Encanto is in danger, Mirabel decides that she, the only ordinary Madrigal, might just be her exceptional family''s last hope.
+
+![Encanto Poster](https://lumiere-a.akamaihd.net/v1/images/p_encanto_homeent_22359_4892ae1c.jpeg)',
+    3
+  );
+INSERT INTO notes (title, content, notebook_id)
+VALUES ('Notebook4 - Note1', '## Notebook4 - Note1', 4);
+INSERT INTO notes (title, content, notebook_id)
+VALUES ('Notebook5 - Note1', '## Notebook5 - Note1', 5);
+INSERT INTO notes (title, content, notebook_id)
+VALUES ('Notebook6 - Note1', '## Notebook6 - Note1', 6);
