9c1cdfac87f8b32481917b5719035a7f65190142
Clifford Tse <TseClifford@gmail.com>
2022-03-29 22:15:44 -0400

    update db seed data

--- backend/db/seeds/02_notebooks.sql
+++ backend/db/seeds/02_notebooks.sql
@@ -1,6 +1,6 @@
 INSERT INTO notebooks (book, user_id) VALUES ('Recipes', 1);
-INSERT INTO notebooks (book, user_id) VALUES ('Diary', 1);
+INSERT INTO notebooks (book, user_id) VALUES ('School Notes', 1);
 INSERT INTO notebooks (book, user_id) VALUES ('Movies', 1);
 INSERT INTO notebooks (book, user_id) VALUES ('Projects', 2);
-INSERT INTO notebooks (book, user_id) VALUES ('ShortNotes', 2);
+INSERT INTO notebooks (book, user_id) VALUES ('Short Notes', 2);
 INSERT INTO notebooks (book, user_id) VALUES ('Drafts', 3);
--- backend/db/seeds/03_notes.sql
+++ backend/db/seeds/03_notes.sql
@@ -73,25 +73,180 @@ Directions:
   );
 INSERT INTO notes (title, content, notebook_id)
 VALUES (
-    'March 20',
-    '## March 20, 2022
-- I wrote some code',
+    'CS100 Notes',
+    '# Lecture notes
+ 
+Syntax highlighting
+ 
+```
+js
+var foo = function (bar) {
+  return bar++;
+};
+ 
+console.log(foo(5));
+```
+ 
+## Tables
+ 
+| Option | Description |
+| ------ | ----------- |
+| data   | path to data files to supply the data that will be passed into templates. |
+| engine | engine to be used for processing templates. Handlebars is the default. |
+| ext    | extension to be used for dest files. |',
     2
   );
 INSERT INTO notes (title, content, notebook_id)
 VALUES (
-    'March 21',
-    '## March 21, 2022
-- I wrote more code',
+    'CS135 Notes',
+    'Command Line Arguments
+----------------------
+
+Example:  ./word-count -b -sort SelectionSort -suf < textfile
+
+-b | -a | -s
+  (required) Specifies the type of tree for storing (word, count) pair
+  possible trees are Binary search tree, Avl tree, and Splay tree
+
+    -b - Count frequencies using an unbalanced binary search tree 
+    -a - Count frequencies using an AVL tree 
+    -s - Count frequencies using a splay tree 
+
+-sort SelectionSort | MergeSort | HeapSort
+  (optional) Specifies the type of sort.  
+  If -sort is omitted, HeapSort is used
+
+-suf
+  (optional) Turns on suffix checker
+
+-------------------------
+Design Decisions & Issues
+-------------------------
+
+Class Hierachy
+
+		        |-------------------------|             |------------|
+                |     BinarySearchTree    |--has-a----->|   BSTNode  |
+                |-------------------------|             |------------|
+                           ^    ^                           ^  ^
+                          /      \                          |  |
+                       is-a     is-a                        |  |
+                        /          \                        |  |
+                       /           |--------------|         |  |
+                      /            |  SplayTree   |-has-a---|  |
+                     /             |--------------|            |
+                    /                                        is-a 
+                   /                                           |
+     |----------------|                                  |------------|
+     |     AVLTree    |--has-a-------------------------->| AVLNode    |
+     |----------------|                                  |------------|
+   ',
+    2
+  );
+INSERT INTO notes (title, content, notebook_id)
+VALUES (
+    'Markdown syntax guide',
+    '# Markdown syntax guide
+
+## Headers
+
+# This is a Heading h1
+## This is a Heading h2 
+###### This is a Heading h6
+
+## Emphasis
+
+*This text will be italic*  
+_This will also be italic_
+
+**This text will be bold**  
+__This will also be bold__
+
+_You **can** combine them_
+
+## Lists
+
+### Unordered
+
+* Item 1
+* Item 2
+* Item 2a
+* Item 2b
+
+### Ordered
+
+1. Item 1
+1. Item 2
+1. Item 3
+  1. Item 3a
+  1. Item 3b
+
+## Images
+
+![This is a alt text.](https://markdownlivepreview.com/image/sample.png "This is a sample image.")
+
+## Links
+
+You may be using [Markdown Live Preview](https://markdownlivepreview.com/).
+
+## Blockquotes
+
+> Markdown is a lightweight markup language with plain-text-formatting syntax, created in 2004 by John Gruber with Aaron Swartz.
+>
+>> Markdown is often used to format readme files, for writing messages in online discussion forums, and to create rich text using a plain text editor.
+
+## Tables
+
+| Left columns  | Right columns |
+| ------------- |:-------------:|
+| left foo      | right foo     |
+| left bar      | right bar     |
+| left baz      | right baz     |
+
+## Blocks of code
+
+```
+let message = '' Hello world '';
+alert(message);
+```
+
+## Inline code
+
+This web site is using `markedjs/marked`.',
     2
   );
 INSERT INTO notes (title, content, notebook_id)
 VALUES (
     'Encanto',
-    '### Storyline
+    '# Encanto
+
+### Storyline
 Encanto tells the tale of an extraordinary family, the Madrigals, who live hidden in the mountains of Colombia, in a magical house, in a vibrant town, in a wondrous, charmed place called an Encanto. The magic of the Encanto has blessed every child in the family with a unique gift from super strength to the power to heal-every child except one, Mirabel. But when she discovers that the magic surrounding the Encanto is in danger, Mirabel decides that she, the only ordinary Madrigal, might just be her exceptional family''s last hope.
 
 ![Encanto Poster](https://lumiere-a.akamaihd.net/v1/images/p_encanto_homeent_22359_4892ae1c.jpeg)',
+    3
+  );
+INSERT INTO notes (title, content, notebook_id)
+VALUES (
+    'The Batman',
+    '# The Batman
+
+### Storyline
+Batman ventures into Gotham City''s underworld when a sadistic killer leaves behind a trail of cryptic clues. As the evidence begins to lead closer to home and the scale of the perpetrator''s plans become clear, he must forge new relationships, unmask the culprit and bring justice to the abuse of power and corruption that has long plagued the metropolis.
+
+![The Batman Poster](https://i.redd.it/jb5aaywg7sy71.jpg)',
+    3
+  );
+INSERT INTO notes (title, content, notebook_id)
+VALUES (
+    'Uncharted
+',
+    '# Uncharted
+
+### Storyline
+Treasure hunter Victor "Sully" Sullivan recruits street-smart Nathan Drake to help him recover a 500-year-old lost fortune amassed by explorer Ferdinand Magellan. What starts out as a heist soon becomes a globe-trotting, white-knuckle race to reach the prize before the ruthless Santiago Moncada can get his hands on it. If Sully and Nate can decipher the clues and solve one of the world''s oldest mysteries, they stand to find $5 billion in treasure -- but only if they can learn to work together.
+
+![Uncharted Poster](https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcS148VQcCwjB4BgkrjdLPF7ln5At6Z6xvLMdS8YSUXoksYExx4R)',
     3
   );
 INSERT INTO notes (title, content, notebook_id)
